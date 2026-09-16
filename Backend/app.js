a8591b6844f5068e221fd060145e1b83dbb07bc1
Meir925man <meirsilverman2@gmail.com>
2026-09-16 09:30:21 +0300

    add: added routes import in app.js and added the the connection to the mongodb data base

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from 'dotenv';
 import "./db/db.js";
 dotenv.config();
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -10,7 +11,7 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(cors({}));
 app.use(express.json());
 
-app.use("/users");
+app.use("/users", userRoutes);
 
 
 app.use((_, res) => {
--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,1 +1,15 @@
+// The connection to mongodb:
 
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URL || "mongodb://localhost:27017");
+
+try {
+    client.connect();
+    console.log("mongodb is now connected"); 
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+};
+
+export const db = client.db("auth-app")
